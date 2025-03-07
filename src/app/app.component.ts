ba9ff66cd079d48bdb23b5df1a90f56c670b7136
alinavovkov <alinvovkov@gmail.com>
2025-03-07 17:52:21 +0200

    the api was connected, basic interface was made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogNewsCardComponent } from './components/dialog-news-card/dialog-news-card.component';
+import { NewsService } from './services/news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // imports: [MatGridListModule],
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  newsArticles: any[] = [];
+
+  constructor(
+    private dialog: MatDialog,
+    private newsService: NewsService) { }
+
+  ngOnInit(): void {
+    this.fetchNews();
+  }
+
   title = 'news-of-the-world';
+
+  fetchNews(): void {
+    this.newsService.getNews().subscribe((response) => {
+      this.newsArticles = response.articles;
+    });
+  }
+
+  openDialog(article: any): void {
+    this.dialog.open(DialogNewsCardComponent,{
+      data: article
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { DialogNewsCardComponent } from './components/dialog-news-card/dialog-news-card.component';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogNewsCardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
